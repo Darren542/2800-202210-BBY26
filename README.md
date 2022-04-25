68c6540c5b41b2fb50ed7f2d98d1146549799fb7
Darren Luck <darrenluck@icloud.com>
2022-04-25 16:35:59 -0700

    Basic Node.js app outline.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-"# 2800_202210_BBY26" 
+## COMP2800 BBY26 Project ##
+
+* [General info](#general-info)
+
+## General Info
+We Not decided anything about this project yet.
+Members:
+* Darren Luck
+
---
+++ index.js
@@ -0,0 +1,35 @@
+// https://expressjs.com/en/guide/routing.html
+
+
+// REQUIRES
+const express = require("express");
+const app = express();
+app.use(express.json());
+const fs = require("fs");
+
+// just like a simple web server like Apache web server
+// we are mapping file system paths to the app's virtual paths
+app.use("/js", express.static("./public/js"));
+app.use("/css", express.static("./public/css"));
+app.use("/img", express.static("./public/img"));
+
+
+app.get("/", function (req, res) {
+    let doc = fs.readFileSync("./app/html/index.html", "utf8");
+
+    // just send the text stream
+    res.send(doc);
+});
+
+// for page not found (i.e., 404)
+app.use(function (req, res, next) {
+    // this could be a separate file too - but you'd have to make sure that you have the path
+    // correct, otherewise, you'd get a 404 on the 404 (actually a 500 on the 404)
+    res.status(404).send("<html><head><title>Page not found!</title></head><body><p>Nothing here.</p></body></html>");
+});
+
+// RUN SERVER
+let port = 8000;
+app.listen(port, function () {
+    console.log("Example app listening on port " + port + "!");
+});
