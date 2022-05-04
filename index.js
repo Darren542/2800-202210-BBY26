b0f6c1060e6071f634da49b0d354a86a7504f97b
Darren Luck <darrenluck@icloud.com>
2022-05-04 13:20:48 -0700

    Initial Commit For Splash Page Feature; Created basic splash.html; Created basic splash.css; Created basic client.css; Modified index.js to load in splash.html.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use("/img", express.static("./public/img"));
 
 
 app.get("/", function (req, res) {
-    let doc = fs.readFileSync("./app/html/index.html", "utf8");
+    let doc = fs.readFileSync("./app/html/splash.html", "utf8");
 
     // just send the text stream
     res.send(doc);
@@ -29,6 +29,11 @@ app.get("/nav", function(req, res) {
 
 app.get("/footer", function(req, res) {
     let doc = fs.readFileSync("./app/templates/footer.html", "utf8");
+    res.send(doc);
+})
+
+app.get("/splash", function(req, res) {
+    let doc = fs.readFileSync("./app/html/splash.html", "utf8");
     res.send(doc);
 })
 
