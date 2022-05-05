d2ecb19a870e24e36f1227e29ed7c55c40ed9dd7
Darren Luck <darrenluck@icloud.com>
2022-05-04 17:16:19 -0700

    Made the login and signup button on Splash Page redirect to correct pages.

---
+++ public/js/splash.js
@@ -0,0 +1,9 @@
+document.querySelector("#log-in").addEventListener("click", () => {
+    window.location.href = "/login";
+    console.log("Button clicked");
+})
+
+document.querySelector("#sign-up").addEventListener("click", () => {
+    window.location.href = "/signup";
+    console.log("Button clicked");
+})
