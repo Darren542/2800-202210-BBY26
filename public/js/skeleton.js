bdc5b41051e03ecc2b3bcbf765f489b43b2dd41a
Darren542 <Darrenluck@icloud.com>
2022-05-05 16:53:22 -0700

    Updated the navbar to have hamburger menu and logo.

--- public/js/skeleton.js
+++ public/js/skeleton.js
@@ -2,8 +2,25 @@
 // This function loads the parts of your skeleton 
 // (navbar, footer, and other things) into html doc. 
 //---------------------------------------------------
-function loadSkeleton(){
-    console.log($('#navbar-placeholder').load('/nav'));
-    console.log($('#footer-placeholder').load('/footer'));
+async function loadSkeleton(){
+    await console.log($('#navbar-placeholder').load('/nav'));
+    await console.log($('#footer-placeholder').load('/footer'));
+    
+
+    
 }
 loadSkeleton();  //invoke the function
+
+$(document).ready(function() {
+    document.querySelector("#logo-div").addEventListener("click", () => {
+        window.location.href = "/";
+        console.log("Button clicked");
+    })
+    let mobileNav =  document.querySelector("#nav-items");
+    document.querySelector("#hamburger-menu").addEventListener("click", () => {
+        mobileNav.classList.toggle('hidden')
+        mobileNav.classList.toggle('flex')
+        console.log("Button clicked");
+    })
+});
+
