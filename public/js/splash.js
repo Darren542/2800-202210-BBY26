fc0c4a3568be70c1c46678301ebf9cdd0e07e010
Darren Luck <darrenluck@icloud.com>
2022-05-04 17:34:47 -0700

    Touched up appearence of Splash page; made the popular event div redirect to event page.

--- public/js/splash.js
+++ public/js/splash.js
@@ -7,3 +7,8 @@ document.querySelector("#sign-up").addEventListener("click", () => {
     window.location.href = "/signup";
     console.log("Button clicked");
 })
+
+document.querySelector("#popular-event").addEventListener("click", () => {
+    window.location.href = "/event";
+    console.log("Button clicked");
+})
