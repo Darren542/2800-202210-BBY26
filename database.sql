c73306bd2ed6636bb6599024389a3f32d21fa608
Brian Cherng <briancherngsch@gmail.com>
2022-05-06 16:24:41 -0700

    changed database and table names

--- database.sql
+++ database.sql
@@ -1,8 +1,8 @@
-CREATE database IF NOT EXISTS bby26;
+CREATE database IF NOT EXISTS BBY_26;
 
-USE bby26;
+USE BBY_26;
 
-CREATE table IF NOT EXISTS users(
+CREATE table IF NOT EXISTS BBY_26_users(
 	userID int NOT NULL AUTO_INCREMENT PRIMARY KEY, 
 	username varchar(16),
 	email varchar(50),
@@ -10,22 +10,22 @@ CREATE table IF NOT EXISTS users(
 	isAdmin boolean DEFAULT FALSE
 );
 	
-INSERT INTO users (username, email, pw, isAdmin) 
+INSERT INTO BBY_26_users (username, email, pw, isAdmin) 
 VALUES ("bcherng", "briancherngsch@gmail.com", 123, 1);
 
-INSERT INTO users (username, email, pw, isAdmin) 
+INSERT INTO BBY_26_users (username, email, pw, isAdmin) 
 VALUES ("dluck", "dluck4@my.bcit.ca", 123, 1);
 
-INSERT INTO users (username, email, pw, isAdmin) 
+INSERT INTO BBY_26_users (username, email, pw, isAdmin) 
 VALUES ("ajand", "aryan.jand@gmail.com", 123, 1);
 
-INSERT INTO users (username, email, pw, isAdmin) 
+INSERT INTO BBY_26_users (username, email, pw, isAdmin) 
 VALUES ("psidhul", "sidhupahul0@gmail.com", 123, 1);
 
-INSERT INTO users (username, email, pw, isAdmin) 
+INSERT INTO BBY_26_users (username, email, pw, isAdmin) 
 VALUES ("gsingh", "germanpreet3533@gmail.com", 123, 1);
 
-INSERT INTO users (username, email, pw) 
+INSERT INTO BBY_26_users (username, email, pw) 
 VALUES ("testuser", "notreal@gmail.com", 123);
 
 
