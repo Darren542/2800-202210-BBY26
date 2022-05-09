d1a81f429e8403a9700f863cdea8612e427772ef
Brian Cherng <briancherngsch@gmail.com>
2022-05-09 13:40:00 -0700

    Changed user info on admin dashboard from table format to card format

--- public/js/admin.js
+++ public/js/admin.js
@@ -9,29 +9,44 @@ function ready() {
     getname.open("GET", "/username", true);
     getname.send();
 
-
+    document.getElementById("card-loader").style = "display: grid; grid-template-columns: 1fr 1fr 1fr; padding: 2px;";
     const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var data = JSON.parse(this.responseText);
             let index = 0;
+            let col = 1;
             data.forEach(function(){
-                let row = document.createElement("tr");
-                let username = document.createElement("td");
-                let email = document.createElement("td");
-                let isAdmin = document.createElement("td");
-                username.innerHTML = data[index].username;
-                email.innerHTML = data[index].email;
+                let card = document.createElement("div");
+                let username = document.createElement("p");
+                let email = document.createElement("p");
+                let isAdmin = document.createElement("p");
+                let deleteBtn = document.createElement("button");
+                deleteBtn.innerHTML = "Delete User";
+                deleteBtn.addEventListener("click", deleteUser);
+                let modifyBtn = document.createElement("button");
+                modifyBtn.addEventListener("click", modifyPrivilege);
+                card.style = "margin: auto;border: 1px solid black; background: white; width: 30vw; height: auto; grid-column:" + col;
+                col++;
+                if (col > 3){
+                    col = 1;
+                }
+                username.innerHTML = "Username: " + data[index].username;
+                email.innerHTML = "Email: " + data[index].email;
                 if (data[index].isAdmin){
                     isAdmin.innerHTML = "Admin";
+                    modifyBtn.innerHTML = "Demote User";
                 } else {
                     isAdmin.innerHTML = "User";
+                    modifyBtn.innerHTML = "Promote User";
                 }
                 index++;
-                row.append(username);
-                row.append(email);
-                row.append(isAdmin);
-                document.getElementById("table-insertion-point").append(row);
+                card.append(username);
+                card.append(email);
+                card.append(isAdmin);
+                card.append(deleteBtn);
+                card.append(modifyBtn);
+                document.getElementById("card-loader").append(card);
             });
         }
     }
@@ -40,3 +55,12 @@ function ready() {
 }
 
 ready();
+
+
+function deleteUser(){
+    console.log("delete");
+}
+
+function modifyPrivilege(){
+    console.log("modify");
+}
