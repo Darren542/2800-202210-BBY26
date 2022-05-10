75ccde6f3823a56c145e689f5a4137d6d4c1a8c7
Brian Cherng <briancherngsch@gmail.com>
2022-05-10 16:54:21 -0700

    changed cards on admin dashboard to use grid, changed layout of information on cards

--- public/js/admin.js
+++ public/js/admin.js
@@ -25,20 +25,25 @@ function ready() {
                 card.id = data[index].username;
                 card.classList.add('card')
                 let username = document.createElement("p");
+                username.style = "grid-row: 2 / span 1; column: 2 / span 1;";
                 let email = document.createElement("p");
+                email.style = "grid-row: 3 / span 1; column: 2 / span 1;";
                 let isAdmin = document.createElement("p");
+                isAdmin.style = "grid-row: 4 / span 1; column: 2 / span 1;";
                 let deleteBtn = document.createElement("button");
                 deleteBtn.username = data[index].username;
                 deleteBtn.innerHTML = "Delete User";
 
                 deleteBtn.addEventListener("click", deleteUser);
 
+                deleteBtn.style = "margin-left: auto; margin-right: auto; grid-row: 2 / span 1; grid-column: 4 / span 1;"
 
                 let modifyBtn = document.createElement("button");
                 modifyBtn.username = data[index].username;
                 modifyBtn.isAdmin = data[index].isAdmin;
+                modifyBtn.style = "margin-left: auto; margin-right: auto; grid-row: 3 / span 1; grid-column: 4 / span 1;"
                 modifyBtn.addEventListener("click", modifyPrivilege, "username=" + data[index].username + "&isAdmin=" + data[index].isAdmin);
-                card.style = "border: 1px solid black; background: white; width: 30vw; height: auto; grid-column:" + col;
+                card.style = "display: grid; grid-template-columns: 2% 60% 2% 36%; grid-template-rows: 5% 30% 30% 30% 5%; border: 1px solid black; background: white; width: 30vw; height: auto; grid-column:" + col;
                 col++;
                 if (col > 3) {
                     col = 1;
@@ -70,20 +75,20 @@ ready();
 
 
 function deleteUser() {
-    /*
-    some form of confirmation
-    */
-    let params = "username=" + this.username;
-    const xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            location.reload();
+    let text = "This will delete the user.\nOk or Cancel.";
+    if (confirm(text)) {
+        let params = "username=" + this.username;
+        const xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                location.reload();
+            }
         }
+        xhttp.open("POST", "/delete-user");
+        xhttp.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+        xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhttp.send(params);
     }
-    xhttp.open("POST", "/delete-user");
-    xhttp.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-    xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhttp.send(params);
 }
 
 function modifyPrivilege() {
@@ -120,10 +125,9 @@ function shuffle() {
     var index = 0;
     userList.forEach(function () {
         let card = document.getElementById(""+userList[index].username);
-        if (card.style.display == ""){
+        if (card.style.display == "grid"){
             let text = "grid-column:"+col+";";
             card.style.cssText += text;
-            console.log("test");
             col++;
         }
         if (col > 3){
@@ -137,12 +141,10 @@ function searchUser() {
     // Declare variables
     let i, txtValue;
     var input = document.getElementById('searchbar').value.toUpperCase();
-    console.log(input);
-    console.log(userList);
     for (i = 0; i < userList.length; i++) {
         txtValue = userList[i].username;
         if (txtValue.toUpperCase().indexOf(input) > -1) {
-            document.getElementById("" + userList[i].username).style.display = "";
+            document.getElementById("" + userList[i].username).style.display = "grid";
         } else {
             document.getElementById("" + userList[i].username).style.display = "none";
         }
