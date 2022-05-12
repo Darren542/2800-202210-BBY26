1818505360574ccf8057822af239724589a81d19
Darren542 <Darrenluck@icloud.com>
2022-05-11 17:06:14 -0700

    added client and server code for update profile page to update users profile db info

--- index.js
+++ index.js
@@ -136,11 +136,11 @@ app.post("/login", function (req, res) {
     testConnection();
 });
 
-app.get("/username", function (req, res){
+app.get("/username", function (req, res) {
     res.send(req.session.username);
 });
 
-app.get("/email", function (req, res){
+app.get("/email", function (req, res) {
     res.send(req.session.email);
 });
 
@@ -237,7 +237,7 @@ app.get("/profile-info/:id", function (req, res) {
                         }
                         else {
                             if (results[0] != null) {
-                                if (!results[0].showLoc) {
+                                if (!results[0].showLoc && results[0].username != req.session.username) {
                                     results[0].country = 'hidden';
                                     results[0].province = 'hidden';
                                     results[0].city = 'hidden';
@@ -260,19 +260,19 @@ app.get("/profile-info/:id", function (req, res) {
     testConnection();
 });
 
-app.get("/dogs", function (req, res){
+app.get("/dogs", function (req, res) {
     res.send("dog section is still under construction");
 });
 
-app.get("/photos", function (req, res){
+app.get("/photos", function (req, res) {
     res.send("photos section is still under construction");
 });
 
-app.get("/my-events", function (req, res){
+app.get("/my-events", function (req, res) {
     res.send("events section is still under construction");
 });
 
-app.get("/my-groups", function (req, res){
+app.get("/my-groups", function (req, res) {
     res.send("groups section is still under construction");
 });
 
@@ -293,7 +293,7 @@ app.get("/footer", function (req, res) {
 
 
 app.get("/splash", function (req, res) {
-    if (req.session.loggedIn){
+    if (req.session.loggedIn) {
         res.redirect("/");
     }
     let doc = fs.readFileSync("./app/html/splash.html", "utf8");
@@ -311,12 +311,12 @@ app.get("/search", function (req, res) {
 })
 
 app.get("/create", function (req, res) {
-    if (req.session.loggedIn){
+    if (req.session.loggedIn) {
         let doc = fs.readFileSync("./app/html/create.html", "utf8");
         res.send(doc);
     } else {
         res.redirect("/");
-    }    
+    }
 })
 
 
@@ -349,7 +349,7 @@ app.post("/add-user", function (req, res) {
                     if (error) {
                     }
                     if (results && results[0] != null) {
-                        if ( results[0].username == req.body.username) {
+                        if (results[0].username == req.body.username) {
                             res.send({ status: "ufailure", msg: "Username Taken" });
                         } else if (results[0].email == req.body.email) {
                             res.send({ status: "efailure", msg: "Email Taken" });
@@ -380,14 +380,73 @@ app.post("/add-user", function (req, res) {
     tryConnection()
 });
 
-app.get("/edit-profile", function (req, res) {
-    if (req.session.loggedIn){
+app.get("/edit-profile/", function (req, res) {
+    if (req.session.loggedIn) {
+        res.redirect(`/edit-profile/${req.session.username}`);
+    } else {
+        res.redirect("/");
+    }
+})
+
+app.get("/edit-profile/:id", function (req, res) {
+    if (req.session.loggedIn) {
         let doc = fs.readFileSync("./app/html/user-profile-editor.html", "utf8");
         res.send(doc);
     } else {
         res.redirect("/");
-    }    
+    }
 })
+
+app.post("/update-profile/:id", function (req, res) {
+    // Can only update the profile if you are admin or it is your account
+    if (req.session.loggedIn && (req.session.isAdmin || (req.session.username == req.params.id))) {
+        let connection;
+        let myPromise = new Promise((resolve, reject) => {
+
+            connection = mysql.createConnection({
+                host: "localhost",
+                user: "root",
+                password: "",
+                database: "BBY_26"
+            });
+
+            connection.connect(err => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(true);
+                }
+            });
+
+        });
+
+        myPromise.then(
+            function () {
+                connection.execute(
+                    "UPDATE bby_26_profiles SET displayName = ?, quote = ?, country = ?, province = ?, city = ?, userDescription = ?, showEmail = ?, showLoc = ? WHERE username = ?",
+                    [req.body.displayName, req.body.quote, req.body.country, req.body.province, req.body.city, req.body.description, req.body.showEmail, req.body.showLocation, req.params.id],
+                    function (error, results) {
+                        if (error) {
+                            console.log(error);
+                        }
+                        else {
+                            res.send({ status: "success", msg: "Updated Profile." });
+                        }
+
+                    });
+                connection.end();
+            },
+            function (error) {
+                console.log(error);
+            }
+        );
+
+
+    } else {
+        res.redirect("/");
+    }
+})
+
 
 app.use(function (req, res, next) {
     res.status(404).send("<html><head><title>Page not found!</title></head><body><p>Nothing here.</p></body></html>");
---
+++ public/js/user-profile-editor.js
@@ -0,0 +1,89 @@
+
+//----------------------------------------------------------------------
+// takes the whole path as a parameter.
+// eg. user-profile/testuser
+// Calls displayProfileInfo
+//----------------------------------------------------------------------
+async function getProfileInfo(path) {
+    const requestId = path.substring(path.lastIndexOf('/'));
+    try {
+        let response = await fetch(`/profile-info${requestId}`, {
+            method: 'GET'
+        });
+        if (response.status === 200) {
+            let data = await response.json();
+            displayProfileInfo(data);
+        } else {
+            console.log(response.status);
+        }
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+getProfileInfo(window.location.pathname);
+
+//------------------------------------------------------------------------
+// Inserts all the users current profile information into the input fields
+// gets called by getProfileInfo
+//------------------------------------------------------------------------
+function displayProfileInfo(data) {
+
+    document.getElementById("display-name").value = data.displayName;
+    document.getElementById("user-quote").value = data.quote;
+    document.getElementById("country").value = data.country;
+    document.getElementById("province").value = data.province;
+    document.getElementById("city").value = data.city;
+    document.getElementById("description").value = data.userDescription;
+    if (data.showEmail) {
+        document.getElementById("show-email-yes").checked = true;
+    } else {
+        document.getElementById("show-email-no").checked = true;
+    }
+    if (data.showLoc) {
+        document.getElementById("show-location-yes").checked = true;
+    } else {
+        document.getElementById("show-location-no").checked = true;
+    }
+}
+
+document.querySelector("#update-profile").addEventListener("click", () => {
+    updateProfileInfo();
+});
+
+async function updateProfileInfo() {
+    let profileData = {
+        displayName: document.getElementById("display-name").value,
+        quote: document.getElementById("user-quote").value,
+        country: document.getElementById("country").value,
+        province: document.getElementById("province").value,
+        city: document.getElementById("city").value,
+        description: document.getElementById("description").value,
+        showEmail: document.getElementById("show-email-yes").checked,
+        showLocation:document.getElementById("show-location-yes").checked
+    }
+    
+    let path = window.location.pathname;
+    const requestId = path.substring(path.lastIndexOf('/'));
+    try {
+        let responseObject = await fetch(`/update-profile${requestId}`, {
+            method: 'POST',
+            headers: { "Accept": 'application/json',
+                       "Content-Type": 'application/json'
+            },
+            body: JSON.stringify(profileData)
+        });
+        
+        let parsedJSON = await responseObject.json();
+        
+        if (parsedJSON.status == "success") {
+            document.getElementById("bottom-filler").classList.add("no-show");
+            document.getElementById("update-success").classList.remove("no-show");
+        } else {
+            document.getElementById("bottom-filler").classList.add("no-show");
+            document.getElementById("update-failure").classList.remove("no-show");
+        }
+    } catch(error) {
+        console.log(error);
+    }
+}
