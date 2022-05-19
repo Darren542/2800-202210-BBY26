c034b3a91bc7755fa4757e9d0f2615cb8ae2b171
Darren542 <Darrenluck@icloud.com>
2022-05-19 13:53:59 -0700

    Some changes to the Splash EasterEgg to make dog grow

--- public/js/splash.js
+++ public/js/splash.js
@@ -12,16 +12,14 @@ document.querySelector("#popular-event").addEventListener("click", () => {
 })
 
 function startEasterEgg() {
-    setTimeout(() => {
         let newEle = `<img src="/img/bone.svg" style="cursor: pointer; position: absolute; z-index: 11000; left: 8px; top: 59px; color:white" width="40" height="40" id="bone"></img>`
-        let chaser = `<img src="/img/chaser.png" style="cursor: pointer; position: absolute; z-index: 11000; left: 89px; top: 4.5px;" width="60" height="65" id="chaser"></img>`
+        let chaser = `<img src="/img/chaser.png" style="cursor: pointer; position: absolute; z-index: 11000; left: 89px; top: 4.5px;" width="${dogSize}" height="65px" id="chaser"></img>`
         document.querySelector("#logo-image").src = "/img/logo-transparent-nodog.png";
         document.querySelector("#navbar-placeholder").innerHTML += newEle;
         document.querySelector("#navbar-placeholder").innerHTML += chaser;
         let bone = document.querySelector("#bone");
+        // document.querySelector("#chaser").width = dogSize;        
         
-        
-          
         bone.ondragstart = function() {
             return false;
         };
@@ -31,12 +29,15 @@ function startEasterEgg() {
         bone.addEventListener('mousedown', mousingFunction);  
         
     
-    }, 50);
 }
+
+setTimeout(() => {
+    startEasterEgg();
+}, 50);
 
 let xCoord = 0;
 let yCoord = 0;
-startEasterEgg();
+let dogSize = 60;
 function returnbone(x, y) {
     let chaser = document.querySelector("#chaser");
     let bone = document.querySelector("#bone");
@@ -53,56 +54,97 @@ function returnbone(x, y) {
 
     setTimeout(() => {
         bone.classList.add('no-show');
+        dogSize += 2;
+        // chaser.width = dogSize;
     }, 4000);
 
-    setTimeout(() => {
+    setTimeout(() => {  
         bone.remove();
-        chaser.remove();
+        chaser.remove();     
         startEasterEgg(); 
-    }, 6000);
+    }, 5000);
     
 }
 
 // For the mobile view to move bone
 function touchingFunction(event) {
-    // (1) prepare to moving: make absolute and on top by z-index
+    // let bone = document.querySelector("#bone");
+    // // (1) prepare to moving: make absolute and on top by z-index
+    // bone.style.position = 'absolute';
+    // bone.style.zIndex = 1000;
+
+    // // move it out of any current parents directly into body
+    // // to make it positioned relative to the body
+    // document.body.append(bone);
+
+    // // centers the bone at (pageX, pageY) coordinates
+    // function moveAt(pageX, pageY) {
+    //     bone.style.left = pageX - bone.offsetWidth / 2 + 'px';
+    //     bone.style.top = pageY - bone.offsetHeight / 2 + 'px';
+    // }
+
+    // // move our absolutely positioned bone under the pointer
+    // moveAt(event.pageX, event.pageY);
+
+    // function onMouseMove(event) {
+    //     xCoord = event.touches[0].clientX;
+    //     yCoord = event.touches[0].clientY;
+    //     moveAt(event.touches[0].clientX, event.touches[0].clientY);
+    // }
+
+    // // (2) move the bone on mousemove
+    // document.addEventListener('touchmove', onMouseMove);
+
+    // // // (3) drop the bone, remove unneeded handlers
+    // bone.addEventListener("touchend", (e) => {
+    //     bone.removeEventListener('touchmove', onMouseMove);
+    //     bone.touchend = null;
+    //     returnbone(xCoord, yCoord);
+    // });
+
+    let bone = document.querySelector("#bone");
+    let shiftX = event.touches[0].clientX - bone.getBoundingClientRect().left;
+    let shiftY = event.touches[0].clientY - bone.getBoundingClientRect().top;
+  
     bone.style.position = 'absolute';
     bone.style.zIndex = 1000;
-
-    // move it out of any current parents directly into body
-    // to make it positioned relative to the body
     document.body.append(bone);
-
-    // centers the bone at (pageX, pageY) coordinates
+  
+    moveAt(event.touches[0].clientX, event.touches[0].clientY);
+  
+    // moves the bone at (pageX, pageY) coordinates
+    // taking initial shifts into account
     function moveAt(pageX, pageY) {
-        bone.style.left = pageX - bone.offsetWidth / 2 + 'px';
-        bone.style.top = pageY - bone.offsetHeight / 2 + 'px';
+      bone.style.left = pageX - shiftX + 'px';
+      bone.style.top = pageY - shiftY + 'px';
     }
-
-    // move our absolutely positioned bone under the pointer
-    moveAt(event.pageX, event.pageY);
-
+  
     function onMouseMove(event) {
+        console.log("touchmove");
         xCoord = event.touches[0].clientX;
         yCoord = event.touches[0].clientY;
-        moveAt(event.touches[0].clientX, event.touches[0].clientY);
+      moveAt(event.touches[0].clientX, event.touches[0].clientY);
     }
-
-    // (2) move the bone on mousemove
-    document.addEventListener('touchmove', onMouseMove);
-
-    // // (3) drop the bone, remove unneeded handlers
-    bone.addEventListener("touchend", (e) => {
-        bone.removeEventListener('touchmove', onMouseMove);
-        bone.touchend = null;
-        returnbone(xCoord, yCoord);
-    });
+  
+    // move the bone on mousemove
+    bone.addEventListener('touchmove', onMouseMove);
+    
+    function touchEnd(e) {
+        console.log('touchend');
+            bone.removeEventListener("touchstart", touchingFunction);
+            bone.removeEventListener('touchmove', onMouseMove);
+            bone.removeEventListener('touchend', touchEnd);
+            returnbone(xCoord, yCoord);
+    }
+    // drop the bone, remove unneeded handlers
+    bone.addEventListener("touchend", touchEnd);
 }
 
 // Code for drag and drop from:
 //https://javascript.info/mouse-drag-and-drop
 // for desktop users to move bone
 function mousingFunction(event) {
+    console.log('mouse');
     let bone = document.querySelector("#bone");
     let shiftX = event.clientX - bone.getBoundingClientRect().left;
     let shiftY = event.clientY - bone.getBoundingClientRect().top;
