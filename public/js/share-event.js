cce2b3507023f4b5b3d270776c41b8ace09e78b8
GermanRandhawaa <germanpreet3533@gmail.com>
2022-05-19 11:28:24 -0700

    important changes to share feature and events

--- public/js/share-event.js
+++ public/js/share-event.js
@@ -1,18 +1,23 @@
-const whatsapp = document.querySelector('.whats-app')
-const facebook = document.querySelector('.facebook')
-const twitter= document.querySelector('.twitter')
-const email = document.querySelector('.email')
+const whatsapp = document.querySelector('.whatsapp');
+const telegram  = document.querySelector('.telegram');
+const twitter  = document.querySelector('.twitter')
+const pageurl = location.href;
 
-whatsapp.addEventListener('click',() => {
-    console.log("whats app clicked")
+const message = " This app contains awesome events to socialize and its a lot of fun. Take time to explore them.";
+
+
+const whatsappApi=`https://wa.me/?text-${pageUrl}.${message}`;
+const telegramApi = `https://t.me/share/url?url-${pageUrl}&text-${message}`;
+const twitterApi=`https://twitter.com/intent/tweet?text-${pageUrl}.${message}`;
+
+
+whatsapp.addEventListener("click",() =>{
+    window.open(url = whatsapp, target = "blank" )
+})
+telegram.addEventListener("click",() =>{
+    win
+})
+twitter.addEventListener('click',() =>{
+    
 })
 
-.facebook.addEventListener('click', () => {
-
-})
-.twitter.addEventListener('click', () => {
-
-})
-.email.addEventListener('click',() => {
-
-})
