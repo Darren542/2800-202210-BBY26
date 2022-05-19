5dac88cf97957d254e4eff78f2ac27fd5327f227
Pahul-Sidhu <97401471+Pahul-Sidhu@users.noreply.github.com>
2022-05-18 23:12:39 -0700

    fixed css

--- public/js/group-home.js
+++ public/js/group-home.js
@@ -22,60 +22,90 @@ gettables("/get-tables", function (data) {
         let container = document.createElement("div");
         container.setAttribute("class", "container");
 
-        let name = document.createElement("h3");
-        name.setAttribute("class", "name");
-        name.innerHTML = "Groupname: " + dt[i].name;
-
-        let tags = document.createElement("div");
-        tags.setAttribute("class", "tags");
-
-
-        let type = document.createElement("div");
-        type.setAttribute("class", "type");
-        if (dt[i].type == "free") {
-            type.innerHTML = "Plan: " + "Free";
-        } else {
-            type.innerHTML = "Plan: " + "Premium";
-        }
+        let img = document.createElement("div");
+        img.setAttribute("class", "img");
+        let im = document.createElement("img");
+        im.setAttribute("src", "");
+        im.setAttribute("alt", "GROUP IMAGE");
+        img.append(im);
 
 
-        let desc = document.createElement("div");
-        desc.setAttribute("class", "desc");
-        desc.innerHTML = "description: " +  dt[i].desc
-
+        let details = document.createElement("div");
+        img.setAttribute("class", "details");
+        let groupname = document.createElement("div");
+        groupname.setAttribute("class", "groupname");
+        let head = document.createElement("h3");
+        head.innerHTML = dt[i].name;
+        groupname.append(head);
         let location = document.createElement("div");
         location.setAttribute("class", "location");
-        location.innerHTML = "Location: " + dt[i].city + ", " + dt[i].province + ", " + dt[i].country;
-
-        let groupID = document.createElement("div");
-        groupID.setAttribute("class", "groupID");
-        groupID.innerHTML = "groupID: " + dt[i].groupID;
-
-
-        let owner = document.createElement("div");
-        owner.setAttribute("class", "owner");
-        owner.innerHTML = "owner: " + dt[i].user;
-
-
-        let btn = document.createElement("button");
-        btn.setAttribute("class", "btn");
-        btn.innerHTML = "join";
-
+        location.innerHTML = "Location: " + dt[i].city + ", " + dt[i].province + ", " + dt[i].city;
         let members = document.createElement("div");
         members.setAttribute("class", "members");
         members.innerHTML = "members: " + "under construction";
+        let owjoin = document.createElement("div");
+        owjoin.setAttribute("class", "owjoin");
+        let owner = document.createElement("div");
+        owner.setAttribute("class", "owner");
+        owner.innerHTML = "owner: " + dt[i].user;
+        let d = document.createElement("div");
+        let btn = document.createElement("button");
+        btn.setAttribute("class", "btn");
+        btn.innerHTML = "Join";
+        d.append(btn);
+        owjoin.append(owner);
+        owjoin.append(d);
+        details.append(groupname);
+        details.append(location);
+        details.append(members);
+        details.append(owjoin);
+
+
+        let extra = document.createElement("div");
+        extra.setAttribute("class", "extra");
+        let btns = document.createElement("div");
+        btns.setAttribute("class","btns");
+        let section = document.createElement("div");
+        section.setAttribute("class","section");
+        let description = document.createElement("div");
+        description.setAttribute("class","description");
+        description.innerHTML = dt[i].desc;
+        let b1 = document.createElement("button");
+        b1.setAttribute("class","btn");
+        b1.innerHTML = "description";
+        let b2 = document.createElement("button");
+        b2.setAttribute("class","btn");
+        b2.innerHTML = "members";
+        let b3 = document.createElement("button");
+        b3.setAttribute("class","btn");
+        b3.innerHTML = "events";
+        let b4 = document.createElement("button");
+        b4.setAttribute("class","btn");
+        b4.innerHTML = "photos";
+        let b5 = document.createElement("button");
+        b5.setAttribute("class","btn");
+        b5.innerHTML = "comments";
+
+        section.append(description);
+        btns.append(b1);
+        btns.append(b2);
+        btns.append(b3);
+        btns.append(b4);
+        btns.append(b5);
+
+        extra.append(btns);
+        extra.append(section);
+
+
+
+
 
         let hr = document.createElement("hr");
 
-        container.append(name);
-        container.append(location);
-        container.append(groupID);
-        container.append(type);
-        container.append(owner);
-        container.append(members);
-        container.append(desc);
-        container.append(btn);
-        container.append(hr);
+        container.append(img);
+        container.append(details);
+        container.append(extra);
+        
         document.getElementById("content").append(container);
     }
 
