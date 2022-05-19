104d7633f7079143ebb9de23f93520e5561c6bf3
GermanRandhawaa <germanpreet3533@gmail.com>
2022-05-19 10:21:12 -0700

    adding share feature for events

---
+++ public/js/share-event.js
@@ -0,0 +1,18 @@
+const whatsapp = document.querySelector('.whats-app')
+const facebook = document.querySelector('.facebook')
+const twitter= document.querySelector('.twitter')
+const email = document.querySelector('.email')
+
+whatsapp.addEventListener('click',() => {
+    console.log("whats app clicked")
+})
+
+.facebook.addEventListener('click', () => {
+
+})
+.twitter.addEventListener('click', () => {
+
+})
+.email.addEventListener('click',() => {
+
+})
