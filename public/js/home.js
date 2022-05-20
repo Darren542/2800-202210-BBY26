b5e268efdc507abcb41cd9b5e2d4013fc7934e42
Brian Cherng <briancherngsch@gmail.com>
2022-05-19 18:09:26 -0700

    Added backend code to request for future events and loaded them onto home page

--- public/js/home.js
+++ public/js/home.js
@@ -13,4 +13,45 @@ function ready() {
     getname.send();
 }
 
+
+function loadEvent() {
+    let eventData;
+    const eventDetail = new XMLHttpRequest();
+    eventDetail.open("GET", "/get-events", true);
+    eventDetail.send();
+    eventDetail.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let result = JSON.parse(this.responseText);
+            if (result.status == "fail") {
+                document.getElementById("loader-message").innerHTML = result.msg;
+            } else {
+                eventData = JSON.parse(this.responseText);
+                let temp = document.querySelector('#post-template');
+                let card = temp.content.cloneNode(true);
+                card.id = "post";
+                card.getElementById("event-img").src = "/img/event-imgs/" + eventData[0].event_photo;
+                card.getElementById("event-name-placeholder").innerHTML = eventData[0].event_name;
+                card.getElementById("event-date-placeholder").innerHTML = eventData[0].event_date_time;
+                card.getElementById("event-description-placeholder").innerHTML = eventData[0].event_description;
+                document.getElementById("posts-loader").append(card);
+            }
+
+
+        }
+    };
+
+
+    // const theAddress = new XMLHttpRequest();
+    // theAddress.open("post", "/get-event-address");
+    // theAddress.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    // theAddress.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    // theAddress.send("eventID=" + eventID);
+    // theAddress.onreadystatechange = function() {
+    //     if (this.readyState ==4 && this.status == 200) {
+
+    //     }
+    // )
+}
 ready();
+
+loadEvent();
