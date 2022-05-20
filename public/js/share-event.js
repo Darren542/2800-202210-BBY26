d8b34bff75565591d0fe0298aacab83709e14d36
GermanRandhawaa <germanpreet3533@gmail.com>
2022-05-20 09:22:14 -0700

    for adding share-page through social media in header and footer

--- public/js/share-event.js
+++ public/js/share-event.js
@@ -1,23 +1,6 @@
-const whatsapp = document.querySelector('.whatsapp');
-const telegram  = document.querySelector('.telegram');
-const twitter  = document.querySelector('.twitter')
-const pageurl = location.href;
-
-const message = " This app contains awesome events to socialize and its a lot of fun. Take time to explore them.";
-
-
-const whatsappApi=`https://wa.me/?text-${pageUrl}.${message}`;
-const telegramApi = `https://t.me/share/url?url-${pageUrl}&text-${message}`;
-const twitterApi=`https://twitter.com/intent/tweet?text-${pageUrl}.${message}`;
-
-
-whatsapp.addEventListener("click",() =>{
-    window.open(url = whatsapp, target = "blank" )
-})
-telegram.addEventListener("click",() =>{
-    win
-})
-twitter.addEventListener('click',() =>{
-    
-})
+"use strict"
+async function loadSharePage(){
+    await $('#share-plaveholder').load('/share-page');
+}
+loadSharePage();
 
